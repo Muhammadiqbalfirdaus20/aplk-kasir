492d50d393026433c4486deb1f1e213b7c8218ec
muhammadiqbalfirdaus <miqbalf781@gmail.com>
2025-01-17 09:27:05 +0700

    templating login dan registrasi page , fitur registrasi

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,12 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+route::get('/login', [UserController::class, 'login'])->name('login');
+route::get('/register',[UserController::class,'register'])->name('register');
+route::post('/register',[UserController::class,'registerstore'])->name('register.store');
+route::resource('users',UserController::class);
+
+
